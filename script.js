338852add2a842e06f822cedbaebbf722e1768ec
victorlawrence <nemerem5006@gmail.com>
2025-06-13 09:24:17 +0100

    add project files

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("javascript");
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () =>{
+    menu.classList.toggle("hidden");
+})
